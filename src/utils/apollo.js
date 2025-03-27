27146a43d904ed014589051cb14721de0e95341a
AbhinandanC <nandan22723@gmail.com>
2025-03-27 16:38:33 +0530

    Update icon in Register component

--- src/utils/apollo.js
+++ src/utils/apollo.js
@@ -2,12 +2,15 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import nhost from './nhost';
 
+// Create the basic HTTP link
 const httpLink = createHttpLink({
-  uri: `https://${import.meta.env.VITE_NHOST_SUBDOMAIN}.hasura.app/v1/graphql`,
+  uri: import.meta.env.VITE_HASURA_GRAPHQL_URL,
 });
 
+// Add auth token to every request
 const authLink = setContext((_, { headers }) => {
   const token = nhost.auth.getAccessToken();
+  
   return {
     headers: {
       ...headers,
@@ -16,7 +19,10 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-export const apolloClient = new ApolloClient({
+// Create Apollo Client with auth configuration
+const apolloClient = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
+
+export default apolloClient;
